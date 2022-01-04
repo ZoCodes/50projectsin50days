0addf600ab1374e0eeb0b40a474484341ea763f6
ZoCodes <zoeblogg91@gmail.com>
2022-01-04 17:45:07 +0000

    did the same for the background filter

--- blurry-loading/script.js
+++ blurry-loading/script.js
@@ -14,8 +14,10 @@ function blurring() {
 
     loadText.innerText = `${load}%`
     loadText.style.opacity = scale(load, 0, 100, 1, 0)
+    bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
 }
 
+// function from stackover flow 
 const scale = (num, in_min, in_max, out_min, out_max) => {
     return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
