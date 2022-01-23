bb73ee5a232132354a9c96f6762f286503581ad5
ZoCodes <zoeblogg91@gmail.com>
2022-01-23 18:10:53 +0000

    grabbed what we need in JS

---
+++ testimonials/script.js
@@ -0,0 +1,11 @@
+const testimonialsContainer = document.querySelector('.testimonials-container')
+
+const testimonials = document.querySelector('.testimonial')
+
+const userImage = document.querySelector('.user-image')
+
+const userName = document.querySelector('.username')
+
+const role = document.querySelector('.role')
+
+
